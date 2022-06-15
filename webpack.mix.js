c1448a630aff62f39b24db23c8f99421ec0ba2c5
yt2951 <rahman.yusuftaufiq@gmail.com>
2022-06-15 14:48:28 +0000

    build: update webpack sourcemap and tailwind plugin

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,16 +1,32 @@
+// @ts-check
+
+/**
+ * @typedef {import('laravel-mix')} LaravelMix
+ */
+
+/**
+ * @typedef {Object} Jigsaw
+ * @property {() => LaravelMix} jigsaw
+ */
+
+/**
+ * @type {LaravelMix & Jigsaw}
+ */
+// @ts-ignore
 const mix = require('laravel-mix');
 require('laravel-mix-jigsaw');
 
-mix.disableSuccessNotifications();
-mix.setPublicPath('source/assets/build');
-
-mix.jigsaw()
+mix
+    .jigsaw()
     .js('source/_assets/js/main.js', 'js')
     .css('source/_assets/css/main.css', 'css', [
         require('postcss-import'),
+        require('tailwindcss/nesting'),
         require('tailwindcss'),
     ])
+    .setPublicPath('source/assets/build')
     .options({
         processCssUrls: false,
     })
+    .sourceMaps()
     .version();
