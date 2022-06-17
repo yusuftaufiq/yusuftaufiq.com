b568c992c9f55ea8033ab5b4b29d4e4e91c54321
yt2951 <rahman.yusuftaufiq@gmail.com>
2022-06-17 08:56:30 +0000

    feat(dark-mode): add new dark mode feature

---
+++ source/_assets/js/create-dark-mode.js
@@ -0,0 +1,35 @@
+// @ts-check
+
+const changeToDarkMode = () => {
+  localStorage.theme = 'dark'
+  document.documentElement.classList.add('dark');
+};
+
+const changeToLightMode = () => {
+  localStorage.theme = 'light'
+  document.documentElement.classList.remove('dark');
+};
+
+const changeTheme = () => {
+  if (
+    ('theme' in localStorage === false && window.matchMedia('(prefers-color-scheme: dark)').matches)
+    || localStorage.theme === 'dark'
+  ) {
+    changeToDarkMode();
+  } else {
+    changeToLightMode();
+  }
+};
+
+/**
+ * @type {import('./command').Command}
+ */
+const createDarkMode = {
+  execute: () => {
+    changeTheme();
+    document.querySelector('.js-dark-mode').addEventListener('click', changeToDarkMode);
+    document.querySelector('.js-light-mode').addEventListener('click', changeToLightMode);
+  },
+};
+
+export { createDarkMode };
--- source/_assets/js/main.js
+++ source/_assets/js/main.js
@@ -1,12 +1,13 @@
 // @ts-check
 
+import { createDarkMode } from "./create-dark-mode";
 import { createTypedDescription } from "./create-typed-description";
 import { createTippy } from "./create-tippy";
 import { createSwiper } from "./create-swiper";
 import { createFa } from "./create-fa";
 
 const executeAllCommands = () => {
-  [createTypedDescription, createTippy, createSwiper, createFa].forEach((command) => {
+  [createDarkMode, createTypedDescription, createTippy, createSwiper, createFa].forEach((command) => {
     command.execute()
   });
 };
