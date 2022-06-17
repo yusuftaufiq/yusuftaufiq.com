cd7123b0f53254778a991be870dbf769d8ad173a
yt2951 <rahman.yusuftaufiq@gmail.com>
2022-06-17 08:50:59 +0000

    fix: remove defer to prevent fouc

--- source/_layouts/main.blade.php
+++ source/_layouts/main.blade.php
@@ -18,11 +18,11 @@
   <link rel="canonical" href="{{ $page->getUrl() }}">
   <link rel="stylesheet" href="{{ mix('css/main.css', 'assets/build') }}">
 
-  <script defer src="{{ mix('js/main.js', 'assets/build') }}"></script>
+  <script src="{{ mix('js/main.js', 'assets/build') }}"></script>
 </head>
 
 <body class="h-full bg-gray-100 text-gray-800 leading-normal font-sans antialiased relative m-0 p-0 dark:bg-gray-800 dark:text-gray-100">
-  <div class="swiper h-full w-full">
+  <div class="js-swiper h-full w-full">
     @include('_partials.header')
     <div class="swiper-wrapper">
       @yield('slides')
