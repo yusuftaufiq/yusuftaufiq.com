39ecce193c0bc54bebc429ae25e3c0c4408f8dc6
yt2951 <rahman.yusuftaufiq@gmail.com>
2022-06-17 16:51:42 +0000

    refactor(button): simplify primary and secondary buttons into one file

---
+++ source/_components/button.blade.php
@@ -0,0 +1,64 @@
+@props(['type' => 'primary'])
+
+@php
+  $colors = [
+    'primary' => implode(' ', [
+      'border-gray-100',
+      'text-gray-100',
+      'bg-gray-800',
+      'hover:text-gray-800',
+      'hover:bg-gray-100',
+      'hover:border-gray-800',
+      'active:text-gray-800',
+      'active:bg-gray-100',
+      'active:border-gray-800',
+      'dark:text-gray-800',
+      'dark:bg-gray-100',
+      'dark:hover:text-gray-100',
+      'dark:hover:bg-gray-800',
+      'dark:hover:border-gray-100',
+      'dark:active:text-gray-100',
+      'dark:active:bg-gray-800',
+      'dark:active:border-gray-100',
+    ]),
+    'secondary' => implode(' ', [
+      'border-gray-800',
+      'text-gray-800',
+      'bg-gray-100',
+      'hover:text-gray-100',
+      'hover:bg-gray-800',
+      'hover:border-gray-100',
+      'active:text-gray-100',
+      'active:bg-gray-800',
+      'active:border-gray-100',
+      'dark:text-gray-800',
+      'dark:bg-blue-400',
+      'dark:hover:text-gray-800',
+      'dark:hover:bg-gray-100',
+      'dark:hover:border-gray-800',
+      'dark:active:text-gray-800',
+      'dark:active:bg-gray-100',
+      'dark:active:border-gray-800',
+    ]),
+  ];
+@endphp
+
+<button {{ $attributes->merge([
+  'class' => implode(' ', [
+    'px-8',
+    'py-3',
+    'm-2',
+    'text-lg',
+    'border-2',
+    'font-semibold',
+    'rounded',
+    'transition',
+    'duration-300',
+    'hover:-translate-y-1',
+    'active:-translate-y-1',
+    'rounded-xl',
+    $colors[$type],
+  ])
+]) }}>
+  {{ $slot }}
+</button>
--- source/_sections/home.blade.php
+++ source/_sections/home.blade.php
@@ -1,4 +1,4 @@
-<section id="home" class="container h-full flex flex-col justify-center items-center pt-20 px-6 py-8 mx-auto text-center md:px-10 lg:px-32 xl:max-w-3xl">
+<section class="container h-full flex flex-col justify-center items-center mt-12 px-6 py-8 mx-auto text-center md:px-10 lg:px-32 xl:max-w-3xl">
   <div class="js-typed-strings">
     @foreach ($page->siteSections->home->descriptions as $description)
       <h1 class="text-4xl font-bold leading-none sm:text-5xl">
@@ -14,18 +14,20 @@
   <p class="px-8 mt-8 mb-12 text-lg">Click the continue button below or scroll down to see the next page.</p>
 
   <div class="flex flex-wrap justify-center">
-    <x-button-primary
+    <x-button
+      type="primary"
       class="js-tippy js-dark-mode dark:hidden"
       data-tippy-content="Are you a vampire?"
     >
       <i class="fas fa-moon"></i> Dark mode please
-    </x-button-primary>
-    <x-button-primary
+    </x-button>
+    <x-button
+      type="primary"
       class="js-tippy js-light-mode hidden dark:block"
       data-tippy-content="Let me guess, you are definitely not in a dark room"
     >
       <i class="fas fa-sun"></i> Prefer light mode
-    </x-button-primary>
-    <x-button-secondary>Continue</x-button-primary>
+    </x-button>
+    <x-button type="secondary" class="js-swiper-button-next">Continue</x-button>
   </div>
 </section>
