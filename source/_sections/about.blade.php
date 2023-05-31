842c2a26e133542b7daf1dbf87bb30e786d5d78e
yusuftaufiq <dev.yusuftaufiq@gmail.com>
2023-05-31 13:42:20 +0700

    fix(section): remove image from about me

--- source/_sections/about.blade.php
+++ source/_sections/about.blade.php
@@ -1,10 +1,4 @@
 <x-section class="md:flex-row md:space-x-8">
-  <img
-    loading="lazy"
-    src="{{ $page->siteSections->about->image }}"
-    data-tippy-content="That's me"
-    alt="That's me"
-    class="border-2 border-gray-800 dark:border-gray-100 js-tippy hidden md:block xs-h:block object-cover object-center rounded-full h-44 w-44 sm:h-56 sm:w-56 my-4">
   <div class="flex flex-col space-y-4 text-center my-4">
     <x-section.title class="md:text-right">About Me</x-section.title>
     <p class="text-lg text-justify indent-8">{!! $page->siteSections->about->content !!}</p>
